4fd8a130918184d559dab713b3b47636d4c30025
YeSugarman <y8481656@gmail.com>
2023-08-13 18:31:26 +0300

    add MyVerySecretStruct

--- get_slot_list.cpp
+++ get_slot_list.cpp
@@ -28,6 +28,10 @@ CK_FUNCTION_LIST *p11Func = NULL;
 CK_SLOT_ID *slots = NULL;
 const char *libPath;
 
+typedef struct {
+	char c;
+}MyVerySecretStruct;
+
 
 // This function loads a pkcs11 library. Path of the pkcs11 library is read using P11_LIB environment variable.
 void loadHSMLibrary()
